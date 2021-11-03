41d60a6df43ace363e08abcb37d4a2072f3ca675
Elizabeth <52410353+elizabethfeden@users.noreply.github.com>
2021-11-03 15:48:11 +0300

    Update helpers and visualisation

--- helpers.py
+++ helpers.py
@@ -1,9 +1,15 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import math
 from PIL import Image
-from typing import Tuple, Optional
+from typing import List, Tuple, Optional
+
+
+DATASET_NAMES = [
+      'raw-data-kaggle/images_medseg.npy',
+      'raw-data-kaggle/masks_medseg.npy',
+      'raw-data-kaggle/images_radiopedia.npy',
+      'raw-data-kaggle/masks_radiopedia.npy',
+]
 
   
 def load_datasets_by_names(
@@ -26,20 +32,35 @@ def load_datasets_by_names(
       for i in range(d.shape[3]):
         d_new[i] = resize_dataset(d[:, :, :, i], size=size)
       d = d_new
+    else:
+      d = d.reshape(np.array(d.shape)[[3, 0, 1, 2]])
     
     datasets.append(d)
+    print(f'Added {filename}, shape {d.shape}')
   return datasets
 
 
-def load_datasets(size=None):
-  return load_datasets_by_names([
-      'raw-data-kaggle/images_medseg.npy',
-      'raw-data-kaggle/masks_medseg.npy',
-      'raw-data-kaggle/images_radiopedia.npy',
-      'raw-data-kaggle/masks_radiopedia.npy'
-      ], size=size)
+def load_datasets(prefix_path: str,
+                  size: Optional[Tuple[int, int]] = None
+                  ) -> List[np.ndarray]:
+  return load_datasets_by_names(
+      [prefix_path + name for name in DATASET_NAMES],
+      size=size)
 
 
-def resize_dataset(d, size=(256, 256)):
+def resize_dataset(dataset: np.ndarray,
+                   size: Tuple[int, int] = (256, 256)
+                   ) -> np.ndarray:
+    """Resizes all the images in dataset."""
     return np.array([np.array(Image.fromarray(image).resize(size))
-                     for image in d])
+                     for image in dataset])
+
+def show_im_row(images: np.ndarray, size: int = 10) -> None:
+    """Shows images in a row."""
+    n = images.shape[0]
+    figure, axes = plt.subplots(1, n, figsize=(n * size, size))
+    for ax, image in zip(axes, images):
+        ax.imshow(image, cmap='Greys')
+        ax.set_xticks([])
+        ax.set_yticks([])
+    plt.show()
